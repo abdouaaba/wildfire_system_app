7aa3cc26cefa3b2d1ccf1b182c13bda68be812e6
abdouaaba <abdouaaba11@gmail.com>
2023-03-23 23:24:37 +0000

    update app.py

--- app.py
+++ app.py
@@ -72,10 +72,10 @@ def main():
             image = load_img(uploaded_file, target_size=target)
             
         elif uploaded_file is None and model_selection == "Risk Prediction":
-            load_dotenv()
+            # load_dotenv()
             # Predict using coordinates
-            api_key = os.environ.get("api_key")
-            # api_key = st.secrets["api_key"]
+            # api_key = os.environ.get("api_key")
+            api_key = st.secrets["api_key"]
             center = str(longitude) + ',' + str(latitude)
             rest = ',15,0/350x350?access_token=' + api_key + '&logo=false&attribution=false'
             url = 'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/' + center + rest
